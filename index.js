71e6c9e4de0807549020fba7d4b48781e187b441
ChristelleMeyer <106263939+ChristelleMeyer@users.noreply.github.com>
2022-09-16 16:35:26 -0500

    added html, css and js files in the repository

---
+++ index.js
@@ -0,0 +1,45 @@
+// javascript
+let hscore = 0
+let gscore = 0
+let homeScore = document.getElementById(id="home-score")
+let guestScore = document.getElementById(id="guest-score")
+function add1PointH(){ 
+    hscore = hscore + 1
+    homeScore.innerHTML = hscore;
+}
+
+function add2PointsH(){ 
+    hscore = hscore + 2
+    homeScore.innerHTML = hscore;
+}
+
+function add3PointsH(){ 
+    hscore = hscore + 3
+    homeScore.innerHTML = hscore;
+}
+function add1PointG(){ 
+    gscore = gscore + 1
+    guestScore.innerHTML = gscore;
+}
+
+function add2PointsG(){ 
+    gscore = gscore + 2
+    guestScore.innerHTML = gscore;
+}
+
+function add3PointsG(){ 
+    gscore = gscore + 3
+    guestScore.innerHTML = gscore;
+}
+
+let newGame = document.getElementById(id="new-game")
+
+function resetScore(){
+    
+    guestScore.innerHTML = 0
+    homeScore.innerHTML = 0
+    hscore = 0
+    gscore = 0
+    
+}
+
